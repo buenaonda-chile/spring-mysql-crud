f830335a298e48c0592893904f772bc10af16b04
jhpark@lunasoft.co.kr <qwgh123@naver.com>
2024-01-31 16:07:54 +0900

    Initial commit

---
+++ src/main/kotlin/com/demo/springmysqlcrud/SpringMysqlCrudApplication.kt
@@ -0,0 +1,11 @@
+package com.demo.springmysqlcrud
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringMysqlCrudApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringMysqlCrudApplication>(*args)
+}
---
+++ src/test/kotlin/com/demo/springmysqlcrud/SpringMysqlCrudApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.demo.springmysqlcrud
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringMysqlCrudApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
